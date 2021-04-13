1d75dfb3c17a30b1c55a95e606555609f57528a2
FredrikGithub <72412714+FredrikGithub@users.noreply.github.com>
2021-04-13 14:35:34 +0200

    Dropdown for kategorivalg i schema

--- Eksamen UIN/eksamenuin/schemas/document/Artikler.js
+++ Eksamen UIN/eksamenuin/schemas/document/Artikler.js
@@ -4,26 +4,49 @@ const Artikler = {
     type: 'document',
     fields: [
         {
-            title: 'Tittel',
-            name: 'sidebar',
-            type: 'string'
-        },
-        {
-            title: 'Sidebar content',
-            name: 'sidebar_content',
-            type: 'array',
-            of: [{type: "string"}],
+            title: 'Bilde',
+            name: 'bilde',
+            type: 'image',
+            validation: Rule => Rule.required().error('Dette feltet må utfylles') 
         },
         { 
             title: 'Tittel',
             name: 'tittel',
-            type: 'string'
+            type: 'string',
+            validation: Rule => Rule.required().error('Dette feltet må utfylles') 
+        },
+        { 
+            title: 'Beskrivelse',
+            name: 'beskrivelse',
+            type: 'string',
+            validation: Rule => Rule.required().error('Dette feltet må utfylles') 
         },
         {
             title: 'Innhold',
             name: 'innhold',
             type: 'array',
-            of: [{type: 'block'}]
+            of: [{type: 'block'}],
+            validation: Rule => Rule.required().error('Dette feltet må utfylles') 
+        },
+        {
+            title: 'Kategori',
+            name: 'kategori',
+            type: 'string',
+            options: {
+              list: [ 
+                { title: 'Tjenester', value: 'tjenester',  },
+                { title: 'Sortering', value: 'sortering',  },
+                { title: 'Nyheter', value: 'nyheter',  },
+              ],
+            },
+        },
+        {   
+            title: 'Nokkelord',
+            name: 'nokkelord',
+            type: "array",
+            validation: Rule => Rule.required().error('Dette feltet må utfylles'),
+            of: [{type: "string"}]
+            
         }
 
 
