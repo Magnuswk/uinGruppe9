e95059cbae4aa6e194013d8d90f9b1c10654a7ce
lommerusk113 <mrjoakim112@gmail.com>
2021-04-16 01:31:52 +0200

    Sidebar fikset

--- Eksamen UIN/project/client/src/pages/Artikler.jsx
+++ Eksamen UIN/project/client/src/pages/Artikler.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Tjenester from "../components/Tjenester"
-import Sortering from '../components/Sortering'
 import Artikkelmain from "../components/Artikkelmain"
 import artikkelfetch from '../utils/artikkelService'
 import Sidebar from "../components/Sidebar"
@@ -37,21 +35,21 @@ const Containerleie = () => {
     }, [data?.kategori]);
     
     let arr = []
+    let links = []
 
     for (let i = 0; i < sidebar?.length; i++) {
-        arr.push(sidebar[i].tittel)
+        if (sidebar[i].tittel !== sidebar[i].kategori){
+            arr.push(sidebar[i].tittel);
+            let temp = sidebar[i].tittel;
+            temp = temp.toLowerCase()
+            temp = temp.charAt(0).toUpperCase() + temp.slice(1)
+            temp = temp.replaceAll(" ", "-")
+            links.push(temp)
+        }
       }
-
-    
-    
-
-
-
-
-    if(data?.kategori.toLowerCase()==='tjenester'){
         return(
             <>    
-                <Sidebar/>
+                <Sidebar name={arr} lnk={links} kat={data?.kategori}/>
                 <Artikkelmain >
                     <h1>
                         {data?.tittel}
@@ -63,27 +61,6 @@ const Containerleie = () => {
             </>
         )
 
-    }
-    else if(data?.kategori.toLowerCase()==='sortering'){
-        return(
-            <>    
-                <Sortering />
-                <Artikkelmain >
-                    <h1>
-                        {data?.tittel}
-                    </h1>
-                    <img src={data?.bilde.asset.url} alt='yeetum'></img>
-                    <BlockContent blocks={data?.body}/>
-                </Artikkelmain>
-            </>
-        )
-    }
-    else{
-        return(
-        <Artikkelmain>
-            <h1>Loading...</h1>
-        </Artikkelmain>)
-    }   
 }
 export default Containerleie
  /* {JSON.stringify(data.bilde.asset.url)} */
