2efe97456a2c7f925b88406e168b751941ee4825
FredrikGithub <72412714+FredrikGithub@users.noreply.github.com>
2021-04-24 00:38:53 +0200

    endret til styled component på footer

--- Eksamen UIN/project/client/src/components/Footer.js
+++ Eksamen UIN/project/client/src/components/Footer.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 const Footerstyle = styled.footer`
-    background-color: "#93ba3d";
-    font-Size: "20px";
-    color: "white";
-    border-Top: "1px solid #E7E7E7";
-    text-Align: "center";
-    padding: "20px";
-    left: "0";
-    bottom: "0";
-    height: "75px";
-    width: "100%";
-    overflow: "hidden";
-    margin-top: "5rem";
+    background-color: #93ba3d;
+    font-Size: 20px;
+    color: black;
+    border-Top: 1px solid #E7E7E7;
+    text-Align: center;
+    padding: 20px;
+    padding-bottom: 20px;
+    border: solid;
+    left: 0;
+    bottom: 0;
+    height: 100px;
+    overflow: hidden;
+    margin-top: 5%;
+    display: grid;
 `
-const Footer = ({children}) =>  {
-    return (
-        <>
-            <Footerstyle>{children}</Footerstyle>
-            <p></p>
-        </>
-    );
+const Footer = () =>  {
+
+        return (
+            <>
+                <Footerstyle>
+                    <h1>
+                    <p>Kontakt oss:  </p>
+                    <p>© ØSTBØ AS   </p>
+                    <p>Plassen 5, 8006 Bodø </p>
+                    <p>Postadresse: Postboks 1447, 8038 Bodø </p>
+                    <p>Telefon +47 75 50 09 00</p>
+                    <p>Epost postmottak@ostbo.no</p>
+                    <p>Org.nr 920 508 324</p>
+                    <p>En del av SB-konsernet</p>
+                    <br></br>
+                    </h1>     
+                </Footerstyle>
+            </>
+        );
 }
 
 
