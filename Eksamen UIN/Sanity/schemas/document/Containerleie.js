9a9bb3e4081280662c2a211de2d475458f6469f7
FredrikGithub <72412714+FredrikGithub@users.noreply.github.com>
2021-04-26 13:53:11 +0200

    Opprette skjemaer i sanity

---
+++ Eksamen UIN/Sanity/schemas/document/Containerleie.js
@@ -0,0 +1,105 @@
+const Containerleie = {
+    title: "Containerleie",
+    name: "containerleie",
+    type: "document",
+    fields: [
+        {
+        title: 'Avdeling',
+        name: 'avdeling',
+        type: 'string'
+        },
+        {
+        title: "Navn",    
+        name:"name",
+        type: "string"
+        },
+        {
+            title: "Adresse",
+            name:"adresse",
+            type: "string"
+        },
+        {
+            title: "Post nummer",
+            name:"postnummer",
+            type: "string"
+        },
+        {
+            title: "Post sted",
+            name:"poststed",
+            type: "string"
+        },
+        {
+            title: "Epost adresse",
+            name:"epost",
+            type: "string"
+        },
+        {
+            title: "Telefon nummer",
+            name:"telefonnummer",
+            type: "string"
+        },
+        {
+            title: "Faktura til adressen over?",
+            name:"fakturatiladresse",
+            type: "string"
+        },
+        {
+            title: "Faktura mottaker",
+            name:"fakturamottaker",
+            type: "string"
+        },
+        {
+            title: "Faktura adresse",
+            name:"fakturaadresse",
+            type: "string"
+        },
+        {
+            title: "Faktura postnummer",
+            name:"fakturapostnummer",
+            type: "string"
+        },
+        {
+            title: "Faktura poststed",
+            name:"fakturapoststed",
+            type: "string"
+        },   
+        {
+            title: "Faktura epostadresse",
+            name:"fakturaepostadresse",
+            type: "string"
+        }, 
+        {
+            title: "Faktura på post eller epost?",
+            name:"postellerepost",
+            type: "string"
+        }, 
+        {
+            title: "Plasseringsadresse",
+            name:"plasseringsadresse",
+            type: "string"
+        },
+        {
+            title: "Dato for utsetting",
+            name:"datoforutsetting",
+            type: "date"
+        },
+        {
+            title: "Dato for henting",
+            name:"datoforhenting",
+            type: "date"
+        },
+        {
+            title: "Beskriv plasseringen",
+            name:"beskrivplasseringen",
+            type: "text"
+        },
+        {
+            title: "Tilleggsopplysninger",
+            name:"tilleggsopplysninger",
+            type: "text"
+        },     
+         
+    ]
+}
+
+export default Containerleie;
